04d9a3f8e27205583a13aefbe25e02f0aced8a25
MyeongGeun Shin <mg.shin@kaist.ac.kr>
2020-09-21 11:23:48 +0900

    Update install-package.sh

--- install-package.sh
+++ install-package.sh
@@ -1,2 +1,2 @@
-apt install -y cmake python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential gcc-multilib g++-multilib gdb zsh curl
+apt install -y cmake python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential gcc-multilib g++-multilib gdb zsh curl vim
 
